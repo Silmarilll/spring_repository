2ed2710e985e42e770f9da6cf16e6d3b577e8da0
Olga Kozlova <okozlova@kyriba.com>
2018-01-31 18:45:47 +0300

    79: Annotation based aspects

--- aspects_aop_77/src/com/spring/aop/Logger.java
+++ aspects_aop_77/src/com/spring/aop/Logger.java
@@ -1,8 +1,28 @@
 package com.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class Logger {
+	
+	@Pointcut("execution(void com.spring.aop.Camera.snap())")
+	public void cameraSnap() {	
+		//Not executed
+		System.out.println("execution of snap");
+	}
+	
+	@Before(value = "execution(void com.spring.aop.Camera.snap())")
 	public void aboutToTakePhoto() {
 		System.out.println("About to take foto...");
+	}
+	
+	@Before(value = "cameraSnap()")
+	public void aboutToTakePhoto2() {
+		System.out.println("About to take foto...before cameraSnap()");
 	}
 
 }
