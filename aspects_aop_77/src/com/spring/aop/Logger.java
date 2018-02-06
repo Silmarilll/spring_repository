449d0f94cad95865eb169559a75d60931eace6b5
Olga Kozlova <okozlova@kyriba.com>
2018-02-06 15:17:50 +0300

    90: Combining Pointcuts

--- aspects_aop_77/src/com/spring/aop/Logger.java
+++ aspects_aop_77/src/com/spring/aop/Logger.java
@@ -1,6 +1,7 @@
 package com.spring.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -15,11 +16,22 @@ public class Logger {
 		//Not executed
 		System.out.println("pointcut of snap");
 	}
+	
+	@Pointcut("bean(camera)")
+	public void beanCamera() {	
+		//Not executed
+		System.out.println("pointcut of snap");
+	}
 
 	
-	@Before(value = "withinDemo(exposure, aperture)")
+	@Before(value = "beanCamera() && withinDemo(exposure, aperture)")
 	public void withinDemoAdvice(JoinPoint jp, int exposure, double aperture) {
 		System.out.println("************Before demo*************");
 		System.out.printf("exposure %d, aperture %.2f\n", exposure, aperture);
+	}
+	
+	@After(value = "within(com.spring.aop.*) && !@annotation(Deprecated)")
+	public void someAfterAdvice() {
+		System.out.println("************After demo*************");
 	}
 }
